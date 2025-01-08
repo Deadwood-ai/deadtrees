bc800ec3db06a7a033a4c593d78d5bc1f1e7c057
JesJehle <jesvajnajehle@gmail.com>
2025-01-08 15:50:01 +0100

    apply formatter

--- shared/monitoring.py
+++ shared/monitoring.py
@@ -8,13 +8,16 @@ logfire.configure(environment='development' if dev_mode else 'production')
 
 # uncomment here to disable system-metrics logging
 # I am not entirely sure, what exactly this measures inside a docker container
-logfire.instrument_system_metrics({
-    'process.runtime.memory': ['rss', 'vms'],
-    'process.runtime.cpu.utilization': None,
-    'system.disk.io': ['read', 'write'],
-    'system.disk.time': ['read', 'write'],
-    'system.memory.usage': ['available', 'used'] 
-}, base=None)
+logfire.instrument_system_metrics(
+	{
+		'process.runtime.memory': ['rss', 'vms'],
+		'process.runtime.cpu.utilization': None,
+		'system.disk.io': ['read', 'write'],
+		'system.disk.time': ['read', 'write'],
+		'system.memory.usage': ['available', 'used'],
+	},
+	base=None,
+)
 
 # instrument pydantic model validation
 logfire.instrument_pydantic()
