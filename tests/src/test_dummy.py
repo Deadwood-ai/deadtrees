0d6e6be98912d5830d4e14f2c7bc75fb190e8cdf
Mirko Mälicke <mirko@hydrocode.de>
2025-01-14 18:11:19 +0100

    add a test container

--- tests/src/test_dummy.py
+++ tests/src/test_dummy.py
@@ -1,7 +1,14 @@
+import os
+
 import httpx
 
 
+# get some basic config from os variables
+COG_URL = os.environ.get("COG_URL", "http://nginx:80/cogs/v1")
 
-def test_load_cogs():
+def test_load_small_tiff():
+    response = httpx.get(f"{COG_URL}/test-data-small.tif")
+
+    assert response.status_code == 200
     return True
 
